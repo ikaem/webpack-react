3baf6d3a0ca3506419dba6ee36d646644931a4b8
Karlo <marinovic.karlo@gmail.com>
2021-09-29 21:51:51 +0200

    setup server

--- configs/webpack/prod.ts
+++ configs/webpack/prod.ts
@@ -3,11 +3,11 @@ import { Configuration } from 'webpack';
 import { CleanWebpackPlugin } from 'clean-webpack-plugin';
 
 export const prodConfig: Configuration = {
-  mode: 'production',
-  output: {
-    path: resolve(__dirname, '../../dist'),
-    filename: '[name].js',
-    publicPath: '/',
-  },
-  plugins: [new CleanWebpackPlugin()],
+	mode: 'production',
+	output: {
+		path: resolve(__dirname, '../../dist'),
+		filename: '[name].js',
+		publicPath: '/',
+	},
+	plugins: [new CleanWebpackPlugin()],
 };
